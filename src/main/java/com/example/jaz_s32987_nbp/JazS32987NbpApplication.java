f5e0c9ad93c04902e35126182344b4b9f772f5f1
programKernel <s32987@pjwstk.edu.pl>
2026-06-20 14:58:27 +0200

    Initial commit

---
+++ src/main/java/com/example/jaz_s32987_nbp/JazS32987NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s32987_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS32987NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS32987NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jaz_s32987_nbp/JazS32987NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s32987_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS32987NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
